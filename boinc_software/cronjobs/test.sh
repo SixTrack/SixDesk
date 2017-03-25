f17156751cdaeff0d6c11089a7aef4b22c44dfd0
Alessio Mereghetti <amereghe@lxplus019.cern.ch>
2017-03-25 10:43:00 +0100

    updating crontab job for sixtracktest
    In particular:
    * spooldir set to boinctest
    * addition of template for SixOut.zip functionality - trigger from .desc file
    * addition of appname from .desc file
    
    test.sh file modified with some other additional tests

--- boinc_software/cronjobs/test.sh
+++ boinc_software/cronjobs/test.sh
@@ -36,9 +36,32 @@ submit_descfile_new(){ #$1=.desc file
     echo "\${studyname}: ${studyname}"
 }
 
-echo "-- original:"
-submit_descfile /afs/cern.ch/work/b/boinc/boinc/w4_test_hlfast/work/w4_test_hlfast__2__s__62.31_60.32__2_2.5__e3__15_1_sixvf_boinc21.desc
-echo "-- new:"
-submit_descfile_new w4_test_hlfast__2__s__62.31_60.32__2_2.5__e3__15_1_sixvf_boinc21.desc
-echo "-- new (with old input):"
-submit_descfile_new /afs/cern.ch/work/b/boinc/boinc/w4_test_hlfast/work/w4_test_hlfast__2__s__62.31_60.32__2_2.5__e3__15_1_sixvf_boinc21.desc
+test_parse_descFile_fullPath(){
+    echo "-- original:"
+    submit_descfile /afs/cern.ch/work/b/boinc/boinc/w4_test_hlfast/work/w4_test_hlfast__2__s__62.31_60.32__2_2.5__e3__15_1_sixvf_boinc21.desc
+    echo "-- new:"
+    submit_descfile_new w4_test_hlfast__2__s__62.31_60.32__2_2.5__e3__15_1_sixvf_boinc21.desc
+    echo "-- new (with old input):"
+    submit_descfile_new /afs/cern.ch/work/b/boinc/boinc/w4_test_hlfast/work/w4_test_hlfast__2__s__62.31_60.32__2_2.5__e3__15_1_sixvf_boinc21.desc
+}
+
+evaluate_string_in_file(){
+    # $1: filename
+    local __tmpVar=`cat $1`
+    echo "${__tmpVar}"
+    if ${__tmpVar} ; then
+	echo "true"
+    else
+	echo "false"
+    fi
+}
+
+test_flag(){
+    echo "true" > temp.txt
+    evaluate_string_in_file temp.txt
+    echo "false" > temp.txt
+    evaluate_string_in_file temp.txt
+    rm temp.txt
+}
+
+test_flag
