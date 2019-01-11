d1897eefe1e6bee3e5d6d0edfe2c14da5135e82b
Alessio Mereghetti <amereghe@lxplus081.cern.ch>
2019-01-11 17:47:14 +0100

    set_env.sh handles additional files to create/update studies or load them

--- utilities/bash/set_env.sh
+++ utilities/bash/set_env.sh
@@ -551,6 +551,21 @@ else
     # - set further envs
     setFurtherEnvs
 
+    # - additional files:
+    if [ -n "${additionalFilesInp6T}" ] ; then
+        sixdeskInspectPrerequisites ${lverbose} $envFilesPath -s ${additionalFilesInp6T}
+        if [ $? -gt 0 ] ; then
+            sixdeskmess -1 "not all additional input files for sixtrack are in $envFilesPath dir:"
+            for tmpFile in ${additionalFilesInp6T} ; do
+                sixdeskInspectPrerequisites true $envFilesPath -s ${tmpFile}
+            done
+	    sixdeskexit 13
+        fi
+        for tmpFile in ${additionalFilesInp6T} ; do
+            sixdeskmess -1 "user requested additional input file ${tmpFile} for sixtrack jobs"
+        done
+    fi
+
     # - define user tree
     sixdeskDefineUserTree
 
@@ -600,12 +615,26 @@ else
             for tmpFile in htcondor/htcondor_run_six.sub htcondor/htcondor_job.sh ; do
                 [ -e ${sixdeskwork}/`basename ${tmpFile}` ] || cp -p ${SCRIPTDIR}/templates/${tmpFile} ${sixdeskwork}
             done
+            # additional files:
+            if [ -n "${additionalFilesInp6T}" ] ; then
+                sixdeskmess -1 "taking care of additional files for sixtrack jobs"
+                for tmpFile in ${additionalFilesInp6T} ; do
+	            cp ${envFilesPath}/${tmpFile} studies/
+                done
+            fi
 	elif ${lload} ; then
 	    cp ${envFilesPath}/sixdeskenv .
 	    cp ${envFilesPath}/sysenv .
 	    if ${llocalfort3} ; then
 		cp ${envFilesPath}/fort.3.local .
 	    fi
+            # additional files:
+            if [ -n "${additionalFilesInp6T}" ] ; then
+                sixdeskmess -1 "taking care of additional files for sixtrack jobs"
+                for tmpFile in ${additionalFilesInp6T} ; do
+	            cp ${envFilesPath}/${tmpFile} .
+                done
+            fi
 	    sixdeskmess -1 "Switched to study $LHCDescrip"
 	fi
     fi
