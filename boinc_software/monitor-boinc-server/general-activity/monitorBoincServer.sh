8e64596b496e4b7114057d5a74f769e5c432f4f1
Sixtrack runtime <sixtadm@lxplus753.cern.ch>
2020-08-31 17:21:55 +0200

    Solving issue with certificate of server status page

--- boinc_software/monitor-boinc-server/general-activity/monitorBoincServer.sh
+++ boinc_software/monitor-boinc-server/general-activity/monitorBoincServer.sh
@@ -11,16 +11,24 @@ currTim=`echo "${rightNow}" | awk '{print ($2)}'`
 
 #
 echo ""
-echo "new query at: ${rightNow}"
+echo "new query at: ${rightNow} - on host `hostname -s`"
 
 # parse data
 echo " getting report from server and parsing data..."
 EXIT_STATUS=10
+iCount=0
 while [ $EXIT_STATUS -eq 10 ] ; do
+    # export PYTHONHTTPSVERIFY=0  # issue with certificate
     tmpCommand="python parseHTML.py --url ${serverStatusReport} -d ${currDay} -t ${currTim}"
     # echo " running command: ${tmpCommand}"
     ${tmpCommand}
     EXIT_STATUS=$?
+    let iCount=${iCount}+1
+    if [ ${iCount} -ge 10 ] ; then
+	echo " ...tried to download server page but encountered error for too many times! Aborting..."
+	echo " ...done."
+	exit 1
+    fi
     echo " `date` - exit status of parser: $EXIT_STATUS"
     sleep 3
 done
